3763c4406eb7e09e7612a43cd8aa464161c88aec
Kalena A <79993670+cronchyfries@users.noreply.github.com>
2024-05-14 11:05:02 -0400

    Add files via upload

---
+++ RandomCompliment_Part2.java
@@ -0,0 +1,91 @@
+/*
+ * Kalena Abrahim
+ * 4/21/24
+ * Random Compliment 2
+ */
+
+ import java.util.Random;
+ import java.util.Scanner;
+ 
+ public class RandomCompliment_Part2 {
+     public static void main(String[] args) {
+         // 1. Salutation
+         System.out.println("Welcome to the Random Complimenter.\n");
+ 
+         // 2. Scanner instantiation
+         Scanner scanner = new Scanner(System.in);
+ 
+         // 3. Call method to get compliments
+         String[] compliments = getCompliments(scanner);
+ 
+         // 4. Call method to get Random object
+         Random random = getRandom(scanner);
+ 
+         // 5. Call method to output compliments
+         outputCompliments(scanner, random, compliments);
+ 
+         // 6. Valedictory
+         System.out.println("\nHave a great day!");
+ 
+         // 7. Close Scanner
+         scanner.close();
+     }
+ 
+     // Get compliments array
+     public static String[] getCompliments(Scanner scanner) {
+         System.out.println("How many compliments would you like to add?");
+         int numCompliments = getInput(scanner);
+ 
+         // Create compliments array
+         String[] compliments = new String[numCompliments];
+ 
+         for (int i = 0; i < numCompliments; i++) {
+             System.out.println("Please enter compliment #" + (i + 1) + ":");
+             compliments[i] = scanner.nextLine();
+         }
+ 
+         // Print message indicating compliments have been loaded
+         System.out.println("\nThe " + numCompliments + " compliments have been loaded.");
+ 
+         // Return the compliments array
+         return compliments;
+     }
+ 
+     // Get Random object
+     public static Random getRandom(Scanner scanner) {
+         System.out.println("\nEnter a seed for the random number generator (RGN).");
+         int seed = getInput(scanner);
+ 
+         // Return the Random object with the provided seed
+         return new Random(seed);
+     }
+ 
+     // Output random compliments
+     public static void outputCompliments(Scanner scanner, Random random, String[] compliments) {
+         // Get number of compliments
+         int numCompliments;
+ 
+         do {
+             System.out.println("\nHow many compliments would you like? ('0' to quit)");
+             numCompliments = getInput(scanner);
+ 
+             for (int i = 0; i < numCompliments; i++) {
+                 System.out.println(compliments[random.nextInt(compliments.length)]);
+             }
+         } while (numCompliments != 0);
+     }
+ 
+     // Get integer input
+     public static int getInput(Scanner scanner) {
+         return Integer.parseInt(scanner.nextLine());
+     }
+ }
+ 
+ /*
+ 1. How much time (in hours) was required to complete the lab? (CRITICAL QUESTION)
+ About an hour and a half.
+ 2. What was the hardest part of this lab?
+ The compliment output.
+ 3. What will you always remember (never forget) from this exercise?
+ Formatting.
+ */
---
+++ RandomCompliment_Part3.java
@@ -0,0 +1,117 @@
+/*
+ * Kalena Abrahim
+ * 4/26/24
+ * Random Compliment Pt. 3
+ */
+
+ import java.util.Scanner;
+ import java.util.Random;
+ 
+ public class RandomCompliment_Part3 {
+     public static void main(String[] args) {
+         // Initialize scanner
+         Scanner scanner = new Scanner(System.in);
+ 
+         // Print salutation
+         System.out.println("Welcome to the Random Complimenter.\n");
+ 
+         // Call process method to get compliments array
+         String[] compliments = process(scanner);
+ 
+         // Call input method to get random number generator seed
+         Random random = input(scanner);
+ 
+         // Call output method to print random compliments
+         output(scanner, random, compliments);
+
+         // Close scanner
+         scanner.close();
+ 
+         // Print valedictory 
+         System.out.println("\nHave a great day!");
+     }
+ 
+     // Process user input for compliments
+     public static String[] process(Scanner scanner) {
+         // Initialize empty array
+         String[] complimentsArray = new String[0];
+         int count = 0;
+         boolean continueLoop = true;
+ 
+         while (continueLoop) {
+             // Prompt for compliment
+             System.out.print("Please enter compliment #" + (count + 1) +
+                 " or <ENTER> to quit loading compliments:\n");
+             String input = scanner.nextLine();
+ 
+             // Check for empty string
+             if (input.length() == 0) {
+                 continueLoop = false;
+             } else {
+ 
+                 // Increase array size
+                 String[] newArray = new String[complimentsArray.length + 1];
+ 
+                 // Copy compliments to new array
+                 for (int i = 0; i < complimentsArray.length; i++) {
+                     newArray[i] = complimentsArray[i];
+                 }
+ 
+                 // Add new compliment
+                 newArray[count] = input;
+                 count++;
+ 
+                 // Update array reference
+                 complimentsArray = newArray;
+             }
+         }
+ 
+         // Print number of compliments
+         System.out.println("\nThe " + count + " compliment(s) " +
+          "have been loaded.\n");
+ 
+         return complimentsArray;
+     }
+ 
+     // Get random number generator seed
+     public static Random input(Scanner scanner) {
+         // Prompt for seed
+         System.out.print("Enter a seed for the random " +
+          "number generator (RGN).\n");
+ 
+         // Return Random object with seed
+         return new Random(scanner.nextInt());
+     }
+ 
+     // Output random compliments
+     public static void output(Scanner scanner, Random random, String[] compliments) {
+         boolean continueLoop = true;
+ 
+         while (continueLoop) {
+             // Prompt for number of compliments
+             System.out.print("\nHow many compliments would " +
+              "you like? ('0' to quit)\n");
+             int quantity = scanner.nextInt();
+             scanner.nextLine();
+ 
+             if (quantity == 0) {
+                 continueLoop = false;
+             } else {
+                 // Print random compliments
+                 for (int i = 0; i < quantity; i++) {
+                     int index = random.nextInt(compliments.length);
+                     System.out.println(compliments[index]);
+                 }
+             }
+         }
+     }
+ }
+ 
+ /*
+ 1. How much time (in hours) was required to complete the lab?
+  2 hours
+ 2. What was the hardest part of this lab?
+  Making the new array.
+ 3. What will you always remember (never forget) from this exercise?
+  Formatting.
+ */
